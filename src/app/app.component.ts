11cb23e59183998351de46951a2416db9627a815
MishelLiyanage <mailtomishel@gmail.com>
2025-02-23 20:10:58 +0530

    School Dashboard UI DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./features/components/header/header.component";
 import { FooterComponent } from './features/components/footer/footer.component';
+import { SchoolDashboardComponent } from "./features/dashboards/school-dashboard/school-dashboard.component";
 // import { HomeComponent } from "./features/landing/home/home.component";
 // import { PublicationCardComponent } from "./features/components/publication-card/publication-card.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    // RouterOutlet,
     // HomeComponent
     // PublicationCardComponent
-    ,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SchoolDashboardComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
