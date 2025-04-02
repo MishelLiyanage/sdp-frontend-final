d16adb2a5b466b1b3b810e2256f8037ff8837d0c
MishelLiyanage <mailtomishel@gmail.com>
2025-04-02 18:43:18 +0530

    did some UI changes - scaled down the size

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private apiUrl = 'http://localhost:8083/employees'; // Replace with your actual API endpoint
+  private apiUrl = 'http://localhost:8083/employee'; // Replace with your actual API endpoint
 
   constructor(private http: HttpClient) {}
 
+  private getHeaders(): HttpHeaders {
+      const token = localStorage.getItem('accessToken');
+      if (!token) {
+        console.error('No token found in localStorage!');
+        throw new Error('Authentication token not found');
+      }
+      return new HttpHeaders({ Authorization: `Bearer ${token}` });
+    }
+
   getEmployees(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(`${this.apiUrl}/names-with-department`, { headers: this.getHeaders() });
   }
 }
