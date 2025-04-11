7bc8a88db51c7b8a4b5346eb4baf8931d92ee2cd
MishelLiyanage <mailtomishel@gmail.com>
2025-04-11 15:04:47 +0530

    implemented the offerings part and the moving text

--- src/app/features/public-page/public-page/public-page.component.ts
+++ src/app/features/public-page/public-page/public-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,11 +7,28 @@ import { Router } from '@angular/router';
   templateUrl: './public-page.component.html',
   styleUrl: './public-page.component.scss'
 })
-export class PublicPageComponent {
+export class PublicPageComponent implements AfterViewInit{
   constructor(private router: Router) { }
 
   isMenuOpen = false;
 
+  ngAfterViewInit(): void {
+    const elements = document.querySelectorAll('.animate-on-scroll');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('in-view');
+          observer.unobserve(entry.target); // Animate once
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+
+    elements.forEach(el => observer.observe(el));
+  }
+   
   goToLandingPage(): void {
     try {
       this.router.navigate(['/home/landingPage']);
