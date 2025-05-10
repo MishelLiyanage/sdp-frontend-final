cf2fff420e255b535c66b09ca0cddcbbea78fe47
MishelLiyanage <mailtomishel@gmail.com>
2025-05-10 07:11:48 +0530

    Updated, styled and added analytical charts to the dashboard

--- src/app/features/dashboards/admin-dashboard/admin-dashboard.component.ts
+++ src/app/features/dashboards/admin-dashboard/admin-dashboard.component.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PageStartingContainerComponent } from "../../components/page-starting-container/page-starting-container.component";
-// Angular Chart Component
-import { AgCharts } from 'ag-charts-angular';
-// Chart Options Type Interface
 import { AgChartOptions } from 'ag-charts-community';
 import { MonthlyProcessedOrdersComponent } from "../../components/charts/monthly-processed-orders/monthly-processed-orders.component";
+import { Router } from '@angular/router';
+import { OrderCategoryDistributionComponent } from "../../components/charts/order-category-distribution/order-category-distribution.component";
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
   imports: [
     CommonModule,
-    PageStartingContainerComponent,
-    AgCharts,
-    MonthlyProcessedOrdersComponent
+    MonthlyProcessedOrdersComponent,
+    OrderCategoryDistributionComponent
 ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss',
@@ -30,20 +27,22 @@ export class AdminDashboardComponent {
   grade4scholarship: number = 1234;
   ordersToProcess: number = 1234;
 
-  public chartOptions: AgChartOptions;
-  constructor() {
-    this.chartOptions = {
-      // Data: Data to be displayed in the chart
-      data: [
-        { month: 'Jan', avgTemp: 2.3, iceCreamSales: 162000 },
-        { month: 'Mar', avgTemp: 6.3, iceCreamSales: 302000 },
-        { month: 'May', avgTemp: 16.2, iceCreamSales: 800000 },
-        { month: 'Jul', avgTemp: 22.8, iceCreamSales: 1254000 },
-        { month: 'Sep', avgTemp: 14.5, iceCreamSales: 950000 },
-        { month: 'Nov', avgTemp: 8.9, iceCreamSales: 200000 },
-      ],
-      // Series: Defines which chart type and data to use
-      series: [{ type: 'bar', xKey: 'month', yKey: 'iceCreamSales' }]
-    };
+  constructor(private router: Router) {
   }
+
+  registerSchools() {
+    this.router.navigate(['/auth/register']);
+  }
+
+  trackQPPreparation() {
+    this.router.navigate(['/features/scrumboard']);
+    }
+
+    manageOrders() {
+    this.router.navigate(['/features/manageOrders']);
+    }
+
+    manageSchools() {
+      this.router.navigate(['/features/manageSchools']);
+    }
 }
